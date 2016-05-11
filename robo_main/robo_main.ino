c5cbd2e2af13484a6147735fb4bb2a63e35ba145
Philip Manke <philip.manke@hs-augsburg.de>
2016-05-11 10:54:51 +0200

    added some essential functions

--- robo_main/robo_main.ino
+++ robo_main/robo_main.ino
@@ -1,7 +1,47 @@
+#include "robo_main.h"
+#include "robo_custom.h"
+
+/********************************
+*    Main Arduino Functions     *
+********************************/
+
 void setup() {
-  
+  recieved_data = (int*) calloc(32, sizeof(int));
 }
 
 void loop() {
+  decode_recieved_data();
+  set_motors();
   
 }
+
+/********************************
+*    Functions                  *
+********************************/
+
+void decode_recieved_data(){
+  
+}
+
+void set_motors(int val_l, int val_r){
+  
+}
+
+void set_leds(unsigned int val){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
