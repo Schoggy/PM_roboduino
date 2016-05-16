55c6eaa3b33e3938b251046d33654c1b93c25a45
Philip Manke <philip.manke@hs-augsburg.de>
2016-05-16 18:59:46 +0200

    fleshed out decode_received_data; function pointer array added

--- robo_main/robo_main.ino
+++ robo_main/robo_main.ino
@@ -1,35 +1,114 @@
 #include "robo_main.h"
 #include "robo_custom.h"
 
-/********************************
-*    Main Arduino Functions     *
-********************************/
+#define PACKET_SIZE 8 // number of bytes per packet
+#define BUF_SIZE_B (rd_buf_size * PACKET_SIZE) // buffer size in bytes
 
-void setup() {
-  received_data = (int*) calloc(32, sizeof(int));
-}
+/*********************************
+ *    Global Variables           *
+ *********************************/
 
-void loop() {
-  decode_recieved_data();
-  set_motors();
+// data transmission
+char* rdata;                // data sent to the robot
+unsigned int rdata_pos_r;   // position of the read pointer
+unsigned int rdata_pos_w;   // position of the write pointer
+unsigned int rd_buf_size;   // size of the received data buffer in packets
+unsigned int data_pending;  // number of data packets waiting to be decoded
+
+// current status
+int motor_speed_r, motor_speed_l; 
+bool leds_front;
+
+
+// function array: all functions have the return type of void and a char pointer as the parameter
+
+typedef void (*fptr_arr)(char*);
+fptr_arr functions[256] = {NULL};
+
+
+/*********************************
+ *    Main Arduino Functions     *
+ *********************************/
+
+void setup() { // the Arduino will run this function once when it is initially powered on
+  rd_buf_size = 1024;
+  rdata = (char*) calloc(rd_buf_size * PACKET_SIZE, sizeof(char));
+  rdata_pos_r = rdata_pos_w = 0;
   
-}
-
-/********************************
-*    Functions                  *
-********************************/
-
-void decode_received_data(){
+  motor_speed_r = motor_speed_r = 0;
   
+  leds_front = false;
+  
+  // setup for automatic functions (function pointer array)
+  // syntax: functions[function index (0 to 255)] = &function_name;
+  // the first byte send to the robot is the function index.
+  // the next 7 bytes data for the function.
+
+  functions[0] = &set_leds;
+  functions[1] = &update_motor_steering;
+
 }
 
-void set_motors(int val_l, int val_r){
-  
+void loop() { // the Arduino will loop this function forever
+  decode_received_data();
+
 }
 
-void set_leds(unsigned int val){
-  
+/*********************************
+ *    Functions                  *
+ *********************************/
+
+void decode_received_data(){ // read and decode data packets sent to the robot
+  while(data_pending > 0){
+    functions[*rdata[rdata_pos_r]](rdata[rdata_pos_r]); // call function
+    
+    rdata_pos_r = (rdata_pos_r + 8) % BUF_SIZE_B;
+    data_pending--;
+  }
 }
+
+void set_motors(){
+  // speed of the individual motors will be set with this function.
+}
+
+void send_data(char* data){
+  // send data back to the controller
+}
+
+/*********************************
+ *    Interrupt Service Routines *
+ *********************************/
+
+void receive_data(){
+  //write data into buffer
+  //*rdata[rdata_pos_w] = received_data;
+
+  // advance write pointer by one byte in the ring buffer
+  rdata_pos_w = (rdata_pos_w + 1) % BUF_SIZE_B; 
+  data_pending++;
+}
+
+/*********************************
+ *    Automatic Functions        *
+ *********************************/
+
+/* these functions need some setting up, but can then be called automatically,
+ * when the robot receives a certain data packet.
+ * all automatic functions must have the return type void and a single char* as their parameter
+ * when called they will be given the address of the data packet that called them in the char*,
+ * so they can access the data that called them.
+ */
+
+void set_leds(char* input){
+  // update the state of the GPIO pins controlling the LEDS of the robot
+}
+
+void update_motor_steering(char* input){
+  // calculate new values for the motors
+}
+
+
+
 
 
 
