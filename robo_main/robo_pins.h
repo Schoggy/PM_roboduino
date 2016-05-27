d8dff6f435ac1d3c0cc74a0ddabb2aa8187b4256
farolas <daniel.ziganek@kabelmail.de>
2016-05-27 15:21:22 +0200

    motorsteuerung pin-belegung

--- robo_main/robo_pins.h
+++ robo_main/robo_pins.h
@@ -2,8 +2,16 @@
 
 #define LED_STATUS 13
 #define LED_FRONT 22
+#define MOTOR_L_FOWARD 11
+#define MOTOR_L_BACKWARD 10
+#define MOTOR_R_FORWARD 9
+#define MOTOR_R_BACKWARD 8
 
 void init_pins(void) { // initialisation for pins; called from setup()
   pinMode(LED_STATUS, OUTPUT);
   pinMode(LED_FRONT, OUTPUT);
+  pinMode(MOTOR_L_FOWARD, OUTPUT);
+  pinMode(MOTOR_L_BACKWARD, OUTPUT);
+  pinMode(MOTOR_R_FORWARD, OUTPUT);
+  pinMode(MOTOR_R_BACKWARD, OUTPUT);
 }
